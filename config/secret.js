1573eee362569b0e117d38118b81ccf396bce00f
Brijendra Nag <brijendra98@gmail.com>
2017-07-19 17:24:29 -0400

    All APIs integrations fixed, bot fully functional

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,7 @@
 module.exports = {
   fb_token : 'chat_bot',
-  PAGE_ACCESS_TOKEN : 'EAAB4iZA87jBQBAMrOLKrGyYNBBs7v9DCzIYp5usL9BXTAMyPUZAZAYXMjAAMbTsH6ufeOvpnqCza2hS3SEQss9WFb8FJpefZCTb5fmPJ7szSlDsTJ4HORG25eqCuOB7PQ3AnB21DuXnnzJ6Bba79TVeIgaZABvZBnH3FPxBmy1eXu2lYfaDh1O',
-  api_ai_client_access_token : 'ab47610195bf4ea48314c7e29e3fc7d0'
+  PAGE_ACCESS_TOKEN : 'EAAB4iZA87jBQBAHFDVWjDNFomBZBcYQCBaIruOlQpa75zlvWA3wZBZAZAVHZCBRCELEQZAZCNIqWKNwtiLeEyrlEthEPuOhA0EThyZC7dWXv0vj4XvLkmOqO2ncMaGtamtL3MlTFU8PrgFeAOeZAZCk54BrAP13ZCxhoGGGhhwkYZBDAfaxM42MKDL8NG',
+  api_ai_client_access_token : 'dbf811ba107845178a94d0a70fc8bfbe',
+  google_maps_api_key : 'AIzaSyAZQQk3syacLr6w0SBG5vdFkfjmrNo-MBs',
+  ticketmaster_api_key : 'px1Lr03ZDH4cjwS0FAzqasNeQPqcf6YG'
 };
--- index.js
+++ index.js
@@ -7,107 +7,147 @@ const request = require('request');
 const api_ai = require('apiai')(secret.api_ai_client_access_token);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 const server = app.listen(process.env.PORT || 3000, () => {
-  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
 /* For Facebook Validation */
 app.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] && req.query['hub.verify_token'] === secret.fb_token) {
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    res.status(403).end();
-  }
+    if (req.query['hub.mode'] && req.query['hub.verify_token'] === secret.fb_token) {
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        res.status(403).end();
+    }
 });
-
 
 
 function sendMessage(event) {
-  let sender = event.sender.id;
-  let text = event.message.text;
-  console.log(`lat: ${lat} and lng: ${lng}`);
+    let sender = event.sender.id;
+    let text = event.message.text;
 
-  let apiai = api_ai.textRequest(text, {
-    sessionId: 'unique_id'
-  });
+    let apiai = api_ai.textRequest(text, {
+        sessionId: 'unique_id' // use any arbitrary id
+    });
 
-  apiai.on('response', (response) => {
+    apiai.on('response', (response) => {
+        let aiText = response.result.fulfillment.speech;
 
-    let send_text = response.result.fulfillment.speech;
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {
+                access_token: secret.PAGE_ACCESS_TOKEN
+            },
+            method: 'POST',
+            json: {
+                recipient: {
+                    id: sender
+                },
+                message: {
+                    text: aiText
+                }
+            }
+        }, (error, response) => {
+            if (error) {
+                console.log('Error sending message: ', error);
+            } else if (response.body.error) {
+                console.log('Error: ', response.body.error);
+            }
+        });
+    });
 
-    request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: secret.PAGE_ACCESS_TOKEN},
-    method: 'POST',
-    json: {
-      recipient: {id: sender},
-      message: {text: send_text}
-    }
-  }, function (error, response) {
-    if (error) {
-        console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-        console.log('Error: ', response.body.error);
-    }
-  });
-  });
+    apiai.on('error', (error) => {
+        console.log(error);
+    });
 
-  apiai.on('error', (error) => {
-    console.log(error);
-  });
-
-  apiai.end();
+    apiai.end();
 }
-
-/* Handling Ticketmaster events */
-app.post('/events/:location', (req, res) => {
-  var http = require("https");
-var address = req.params.location;
-//console.log(address);
-
-var options = {
-  "method": "GET",
-  "hostname": "maps.googleapis.com",
-  "port": null,
-  "path": `/maps/api/geocode/json?address=${address}&key=AIzaSyAZQQk3syacLr6w0SBG5vdFkfjmrNo-MBs`,
-  "headers": {
-    "cache-control": "no-cache",
-    "postman-token": "1c77a22a-421d-44f9-2455-484f9194a704"
-  }
-};
-
-var req = http.request(options, function (resu) {
-console.log("________________");
-   console.log(resu);
-console.log("________________");
-   var lat = (resu.results[0].geometry.viewport.northeast.lat + req.results[0].geometry.viewport.southwest.lat)/2;
-   var lng = (resu.results[0].geometry.viewport.northeast.lng + req.results[0].geometry.viewport.southwest.lng)/2;
-
-   res.send({"lat": lat, "lng": lng}); 
-});
-
-});
-
-//Consumer Key	px1Lr03ZDH4cjwS0FAzqasNeQPqcf6YG
-//Consumer Secret	SAhfHs1yp9HZa1ox
-//Key Issued	Tue, 07/18/2017 - 10:19
-//Key Expires	Never
 
 /* Handling all messenges */
 app.post('/webhook', (req, res) => {
-  console.log(req.body);
-  if (req.body.object === 'page') {
-    req.body.entry.forEach((entry) => {
-      entry.messaging.forEach((event) => {
-        if (event.message && event.message.text) {
-          sendMessage(event);
-        }
-      });
-    });
-    res.status(200).end();
-  }
+    console.log(req.body);
+    if (req.body.object === 'page') {
+        req.body.entry.forEach((entry) => {
+            entry.messaging.forEach((event) => {
+                if (event.message && event.message.text) {
+                    sendMessage(event);
+                }
+            });
+        });
+        res.status(200).end();
+    }
 });
 
+/* Handling Ticketmaster events */
+app.post('/get_events', (req, res) => {
+    if (req.body.result.action === 'events') {
+        let address = req.body.result.parameters['address'];
 
+        var options = {
+            method: 'GET',
+            url: 'https://maps.googleapis.com/maps/api/geocode/json',
+            qs: {
+                address: address,
+                key: secret.google_maps_api_key
+            },
+            headers: {
+                'postman-token': '12bbfc8b-4322-e500-640e-0788bc87bcc8',
+                'cache-control': 'no-cache'
+            }
+        };
+
+        request(options, function(error, response, body) {
+            if (error) {
+                return res.status(400).json({
+                    status: {
+                        code: 400,
+                        errorType: 'I failed to look up the address.'
+                    }
+                });
+            }
+            body = JSON.parse(body);
+
+            var lat = (body.results[0].geometry.viewport.northeast.lat + body.results[0].geometry.viewport.southwest.lat) / 2;
+            var lng = (body.results[0].geometry.viewport.northeast.lng + body.results[0].geometry.viewport.southwest.lng) / 2;
+            var location = body.results[0].formatted_address;
+
+            var options = {
+                method: 'GET',
+                url: 'https://app.ticketmaster.com/discovery/v2/events.json',
+                qs: {
+                    apikey: secret.ticketmaster_api_key,
+                    latlong: `${lat},${lng}`,
+                },
+                headers: {
+                    'postman-token': '79a8cda6-7210-b830-e519-9bd8ca142b36',
+                    'cache-control': 'no-cache'
+                }
+            };
+            request(options, function(error, response, body) {
+                body = JSON.parse(body);
+                if (error) {
+                    return res.status(400).json({
+                        status: {
+                            code: 400,
+                            errorType: `No events found in ${location}`
+                        }
+                    });
+                }
+                var msg = `Following events are happening near ${location}:-\n\n`;
+                for (var index in body._embedded.events) {
+                    if (msg.length < 500) {
+                        msg += `${parseInt(index, 10)+1}. ${body._embedded.events[index].name}\n\n`;
+                    }
+                }
+                return res.json({
+                    speech: msg,
+                    displayText: msg,
+                    source: 'Ticketmaster'
+                });
+            });
+        });
+    }
+});
