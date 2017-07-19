bd71432eabb45eb6520aa3755563225b5457fb8c
Brijendra Nag <brijendra98@gmail.com>
2017-07-19 03:39:10 -0400

    Bot works, still integrating Ticketmaster and GMaps API

--- config/secret.js
+++ config/secret.js
@@ -1,4 +1,5 @@
 module.exports = {
-  fb_token : 'KEYWORD',
-  PAGE_ACCESS_TOKEN : 'PAGE_ACCESS_TOKEN'
+  fb_token : 'chat_bot',
+  PAGE_ACCESS_TOKEN : 'EAAB4iZA87jBQBAMrOLKrGyYNBBs7v9DCzIYp5usL9BXTAMyPUZAZAYXMjAAMbTsH6ufeOvpnqCza2hS3SEQss9WFb8FJpefZCTb5fmPJ7szSlDsTJ4HORG25eqCuOB7PQ3AnB21DuXnnzJ6Bba79TVeIgaZABvZBnH3FPxBmy1eXu2lYfaDh1O',
+  api_ai_client_access_token : 'ab47610195bf4ea48314c7e29e3fc7d0'
 };
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const secret = require('./config/secret');
+const request = require('request');
+const api_ai = require('apiai')(secret.api_ai_client_access_token);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,19 +22,28 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-const request = require('request');
+
 
 function sendMessage(event) {
   let sender = event.sender.id;
   let text = event.message.text;
+  console.log(`lat: ${lat} and lng: ${lng}`);
 
-  request({
+  let apiai = api_ai.textRequest(text, {
+    sessionId: 'unique_id'
+  });
+
+  apiai.on('response', (response) => {
+
+    let send_text = response.result.fulfillment.speech;
+
+    request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {access_token: secret.PAGE_ACCESS_TOKEN},
     method: 'POST',
     json: {
       recipient: {id: sender},
-      message: {text: text}
+      message: {text: send_text}
     }
   }, function (error, response) {
     if (error) {
@@ -41,7 +52,48 @@ function sendMessage(event) {
         console.log('Error: ', response.body.error);
     }
   });
+  });
+
+  apiai.on('error', (error) => {
+    console.log(error);
+  });
+
+  apiai.end();
 }
+
+/* Handling Ticketmaster events */
+app.post('/events/:location', (req, res) => {
+  var http = require("https");
+var address = req.params.location;
+//console.log(address);
+
+var options = {
+  "method": "GET",
+  "hostname": "maps.googleapis.com",
+  "port": null,
+  "path": `/maps/api/geocode/json?address=${address}&key=AIzaSyAZQQk3syacLr6w0SBG5vdFkfjmrNo-MBs`,
+  "headers": {
+    "cache-control": "no-cache",
+    "postman-token": "1c77a22a-421d-44f9-2455-484f9194a704"
+  }
+};
+
+var req = http.request(options, function (resu) {
+console.log("________________");
+   console.log(resu);
+console.log("________________");
+   var lat = (resu.results[0].geometry.viewport.northeast.lat + req.results[0].geometry.viewport.southwest.lat)/2;
+   var lng = (resu.results[0].geometry.viewport.northeast.lng + req.results[0].geometry.viewport.southwest.lng)/2;
+
+   res.send({"lat": lat, "lng": lng}); 
+});
+
+});
+
+//Consumer Key	px1Lr03ZDH4cjwS0FAzqasNeQPqcf6YG
+//Consumer Secret	SAhfHs1yp9HZa1ox
+//Key Issued	Tue, 07/18/2017 - 10:19
+//Key Expires	Never
 
 /* Handling all messenges */
 app.post('/webhook', (req, res) => {
